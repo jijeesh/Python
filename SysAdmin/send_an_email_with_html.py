0430dedc3b3ac3452bc840acec33a1cf549ac751
Jijeesh K A <8288856+jijeesh@users.noreply.github.com>
2020-01-22 19:23:20 +0530

    create email with html format from csv file
    reference url
    https://stackoverflow.com/questions/38275467/send-table-as-an-email-body-not-attachment-in-python

---
+++ SysAdmin/send_an_email_with_html.py
@@ -0,0 +1,59 @@
+# smtplib module send mail
+import csv
+from tabulate import tabulate
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMETex
+import smtplib
+
+text = """
+Hello, Friend.
+
+Here is your data:
+
+{table}
+
+Regards,
+
+Me"""
+
+html = """
+<html><body><p>Hello, Friend.</p>
+<p>Here is your data:</p>
+{table}
+<p>Regards,</p>
+<p>Me</p>
+</body></html>
+"""
+
+TO = 'recipient@mailservice.com'
+SUBJECT = 'TEST MAIL'
+TEXT = 'Here is a message from python.'
+
+with open('input.csv') as input_file:
+    reader = csv.reader(input_file)
+    data = list(reader)
+
+text = text.format(table=tabulate(data, headers="firstrow", tablefmt="grid"))
+html = html.format(table=tabulate(data, headers="firstrow", tablefmt="html"))
+
+message = MIMEMultipart(
+    "alternative", None, [MIMEText(text), MIMEText(html,'html')])
+
+# Gmail Sign In
+gmail_sender = 'sender@gmail.com'
+gmail_passwd = 'password'
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmail_sender, gmail_passwd)
+
+
+
+try:
+    server.sendmail(gmail_sender, TO.split(","), message.as_string())
+    print ('email sent')
+except:
+    print ('error sending mail')
+
+server.quit()
